d1930ff5d53a3598391a78ba4bfea316079ddffc
Hernán <hernan.arango@correounivalle.edu.co>
2018-02-01 11:37:18 -0500

    retomando foro

--- backup/moodle2/restore_communityforum_activity_task.class.php
+++ backup/moodle2/restore_communityforum_activity_task.class.php
@@ -30,7 +30,7 @@ require_once($CFG->dirroot . '/mod/forum/backup/moodle2/restore_forum_stepslib.p
  * forum restore task that provides all the settings and steps to perform one
  * complete restore of the activity
  */
-class restore_forum_activity_task extends restore_activity_task {
+class restore_communityforum_activity_task extends restore_activity_task {
 
     /**
      * Define (add) particular settings this activity can have
